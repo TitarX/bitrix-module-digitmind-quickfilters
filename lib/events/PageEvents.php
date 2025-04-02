6b424f1d61a870b9f0565f6c0cd570d0830871aa
Dmitriy Ignatenko <titarx@gmail.com>
2025-04-02 13:22:51 +0400

    Result filter page example

--- lib/events/PageEvents.php
+++ lib/events/PageEvents.php
@@ -11,7 +11,8 @@ class PageEvents
 {
     public static function checkQuickFilter()
     {
-        if ($_SERVER['REQUEST_URI'] == '/123') {
+        // Вывод результата фильтра каталога по URL несуществующего (произвольного) раздела каталога
+        if ($_SERVER['REQUEST_URI'] == '/catalog/dresses2/') {
             $newContentUrl = 'https://bx.site/catalog/sportswear/filter/price-base-from-2428-to-2636/color_ref-is-white/sizes_clothes-is-a11f96c3b88d222460d9796067d28b0c/apply/';
 
             CHTTP::SetStatus('200 OK');
