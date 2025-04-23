bf9e7c53d88d4bc45422eb0f4e7237033d83788c
Dmitriy Ignatenko <titarx@gmail.com>
2025-04-23 23:04:43 +0400

    Set page properties

--- lib/events/PageEvents.php
+++ lib/events/PageEvents.php
@@ -23,6 +23,16 @@ class PageEvents
 
     public static function setMeta()
     {
-        //
+        global $APPLICATION;
+
+        if ($_SERVER['REQUEST_URI'] == '/catalog/sportswear/filter/price-base-from-2428-to-2636/color_ref-is-white/sizes_clothes-is-a11f96c3b88d222460d9796067d28b0c/apply/') {
+            $APPLICATION->SetTitle('Проверка h1'); // h1
+            $APPLICATION->SetPageProperty('title', 'Проверка title'); // title
+            $APPLICATION->SetPageProperty('description', 'Проверка description'); // description
+            $APPLICATION->SetPageProperty('keywords', 'Проверка keywords'); // keywords
+            $APPLICATION->SetPageProperty('canonical', 'Проверка canonical'); // canonical
+
+            $APPLICATION->AddChainItem('Проверка breadcrumb', '/catalog/pants/'); // breadcrumb
+        }
     }
 }
