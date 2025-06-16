ba03220b091000b4f6e77704e83f61f754c4facf
Dmitriy Ignatenko <titarx@gmail.com>
2025-06-17 02:07:27 +0400

    Minor fix

--- lang/ru/install/step1.php
+++ lang/ru/install/step1.php
@@ -1,4 +1,4 @@
 <?php
 
 $MESS['DIGITMIND_QUICKFILTERS_MODULE_INSTALL_NEXT'] = 'Продолжить';
-$MESS['DIGITMIND_QUICKFILTERS_MODULE_CREATE_IBLOCK'] = 'Создать инфоблок при установке модуля';
+$MESS['DIGITMIND_QUICKFILTERS_MODULE_CREATE_IBLOCK'] = 'Создать тип инфоблока и инфоблок "Быстрые фильтры", если он не существует';
