4685491fdda4eb5ad99c5b138093b11f45fec8a2
Dmitriy Ignatenko <titarx@gmail.com>
2025-10-13 00:05:14 +0400

    Helper class for UrlRewrite ready

--- lang/ru/lib/helpers/UrlRewriteHelper.php
+++ lang/ru/lib/helpers/UrlRewriteHelper.php
@@ -1,3 +1,7 @@
 <?php
 
 $MESS['DIGITMIND_QUICKFILTERS_INCLUDE_CURRENT_MODULE_FAIL'] = 'Не удалось подключить модуль digitmind.quickfilters';
+$MESS['DIGITMIND_QUICKFILTERS_URLREWRITE_IS_EXISTS_FAIL'] = 'Не удалось проверить наличие правила UrlRewrite';
+$MESS['DIGITMIND_QUICKFILTERS_URLREWRITE_SAVE_ID_FAIL'] = 'Не удалось сохранить правило UrlRewrite, отсутствует идентификатор';
+$MESS['DIGITMIND_QUICKFILTERS_URLREWRITE_SAVE_FAIL'] = 'Не удалось сохранить правило UrlRewrite';
+$MESS['DIGITMIND_QUICKFILTERS_URLREWRITE_DELETE_FAIL'] = 'Не удалось удалить правило UrlRewrite';
--- lib/helpers/UrlRewriteHelper.php
+++ lib/helpers/UrlRewriteHelper.php
@@ -2,6 +2,7 @@
 
 namespace DigitMind\QuickFilters\Helpers;
 
+use Bitrix\Main\ArgumentNullException;
 use Bitrix\Main\Loader;
 use Bitrix\Main\LoaderException;
 use Bitrix\Main\Localization\Loc;
@@ -17,12 +18,134 @@ try {
 
 class UrlRewriteHelper
 {
+    /**
+     * Возвращает массив полей правила для резервного пути
+     *
+     * @return array Массив полей правила
+     *               ID - идентификатор правила
+     *               CONDITION - условие срабатывания правила (регулярное выражение)
+     *               RULE - правило преобразования
+     *               PATH - путь к файлу, который будет обрабатывать запрос
+     *               SORT - сортировка правила
+     */
+    public static function getDmqFilterRuleFields(): array
+    {
+        return [
+            'ID' => 'digitmind:dmqfilter',
+            'CONDITION' => '#^/dmqfilter/#',
+            'RULE' => '',
+            'PATH' => '/index.php',
+            'SORT' => 0
+        ];
+    }
+
+    /**
+     * Проверяет наличие правила в таблице URLRewrite по его ID
+     *
+     * @param string $ruleId ID правила
+     *
+     * @return bool true - правило с таким ID существует, false - не существует
+     */
     public static function isExists(string $ruleId): bool
     {
-        $getListResult = UrlRewriter::GetList(['ID' => 'bitrix:news']);
-        if(!empty($getListResult))
-        {
-            //
+        $siteId = SITE_ID;
+        $filter = ['ID' => $ruleId];
+
+        try {
+            $getListResult = UrlRewriter::GetList($siteId, $filter);
+        } catch (ArgumentNullException $e) {
+            CAdminMessage::ShowMessage(Loc::getMessage('DIGITMIND_QUICKFILTERS_URLREWRITE_IS_EXISTS_FAIL'));
         }
+
+        if (!empty($getListResult)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Сохраняет правило в таблице URLRewrite
+     *
+     * @param array $fields Массив полей правила
+     *                      ID - идентификатор правила (обязательное поле)
+     *                      CONDITION - условие срабатывания правила (регулярное выражение)
+     *                      RULE - правило преобразования
+     *                      PATH - путь к файлу, который будет обрабатывать запрос
+     *                      SORT - сортировка правила
+     *
+     * @return bool true - сохранение прошло успешно, false - ошибка при сохранении
+     */
+    public static function save(array $fields): bool
+    {
+        $returnResult = false;
+
+        if (empty($fields['ID'])) {
+            CAdminMessage::ShowMessage(Loc::getMessage('DIGITMIND_QUICKFILTERS_URLREWRITE_SAVE_ID_FAIL'));
+        }
+
+        $siteId = SITE_ID;
+        $filter = ['ID' => $fields['ID']];
+
+        try {
+            if (self::isExists($fields['ID'])) {
+                UrlRewriter::update($siteId, $filter, $fields);
+            } else {
+                UrlRewriter::add($siteId, $fields);
+            }
+
+            $returnResult = true;
+        } catch (ArgumentNullException $e) {
+            CAdminMessage::ShowMessage(Loc::getMessage('DIGITMIND_QUICKFILTERS_URLREWRITE_SAVE_FAIL'));
+        }
+
+        return $returnResult;
+    }
+
+    /**
+     * Удаляет правило из таблицы URLRewrite по его ID
+     *
+     * @param string $ruleId ID правила
+     *
+     * @return bool true - удаление прошло успешно, false - ошибка при удалении
+     */
+    public static function delete(string $ruleId): bool
+    {
+        $returnResult = false;
+
+        $siteId = SITE_ID;
+        $filter = ['ID' => $ruleId];
+
+        try {
+            UrlRewriter::delete($siteId, $filter);
+
+            $returnResult = true;
+        } catch (ArgumentNullException $e) {
+            CAdminMessage::ShowMessage(Loc::getMessage('DIGITMIND_QUICKFILTERS_URLREWRITE_DELETE_FAIL'));
+        }
+
+        return $returnResult;
+    }
+
+    /**
+     * Создаёт или обновляет правило для резервного пути
+     *
+     * @return void
+     */
+    public static function saveDmqFilter(): void
+    {
+        $ruleFields = self::getDmqFilterRuleFields();
+        self::save($ruleFields);
+    }
+
+    /**
+     * Удаляет правило для резервного пути
+     *
+     * @return void
+     */
+    public static function deleteDmqFilter(): void
+    {
+        $ruleFields = self::getDmqFilterRuleFields();
+        self::delete($ruleFields['ID']);
     }
 }
